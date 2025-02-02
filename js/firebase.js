c84e02e9dccaa855f92f7908559e6459901f76ca
rhdbh <140407250+rhdbh@users.noreply.github.com>
2025-02-03 01:22:45 +0800

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
 apiKey: "AIzaSyAYkRSxHjVrV9QYk45EDP5K2uHOoW-0pkk",
